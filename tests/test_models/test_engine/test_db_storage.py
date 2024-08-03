d3c4feef42e44a8e7c982720daca21e5a134241e
hyiaman <hyiamosei@gmail.com>
2024-08-03 16:12:25 +0000

    chore:updated db_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Unit Test for BaseModel Class
+initial tests
 """
 import unittest
 from datetime import datetime
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Unit Test for BaseModel Class
+file store test
 """
 import unittest
 from datetime import datetime
